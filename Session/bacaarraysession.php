4f1fa7122d22c29c366c84d324bb951409110f73
wirayantikomang <113044930+wirayantikomang@users.noreply.github.com>
2022-11-10 22:22:22 +0700

    Add files via upload

---
+++ Session/bacaarraysession.php
@@ -0,0 +1,24 @@
+<?php
+    session_start()
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Membaca Array dari dalam Session</title>
+</head>
+<body>
+    <?php
+    if(isset($_SESSION['listBuah'])){
+        $daftarBuah = $_SESSION['listBuah'];
+
+        echo "<h1>Daftar Buah</h1>";
+        echo "<ol>";
+        foreach ($daftarBuah as $buah) {
+            echo "<li>$buah</li>";
+        }
+        echo "</ol>";
+    }
+    ?>
+    
+</body>
+</html>
